6f467a05118df71270a083f6180382117f55b02a
wenqi <wenqi.ma@aculearn.com.cn>
2019-02-27 21:22:23 +0800

    add login page

---
+++ ymjt/web/static/js/home.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.shutter').shutter({
+        isAutoPlay: true, // 是否自动播放
+        playInterval: 3000, // 自动播放时间
+        curDisplay: 3, // 当前显示页
+        fullPage: false // 是否全屏展示
+    });
+});
